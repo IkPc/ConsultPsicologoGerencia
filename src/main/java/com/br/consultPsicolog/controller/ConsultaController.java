f6d635cf4cc9eff9506a7ed6f1956f318e1e29f2
Pabllo Cristian <pabllocristian10lima@hotmail.com>
2023-11-24 23:46:44 -0300

    Commit implementando algumas classes que estavam faltando, projeto quase completo

--- src/main/java/com/br/consultPsicolog/controller/ConsultaController.java
+++ src/main/java/com/br/consultPsicolog/controller/ConsultaController.java
@@ -6,6 +6,7 @@ import com.br.consultPsicolog.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class ConsultaController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Optional<Consulta> findById(@PathVariable Long id) {
         return service.findById(id);
     }
---
+++ src/main/java/com/br/consultPsicolog/enuns/RoleName.java
@@ -0,0 +1,7 @@
+package com.br.consultPsicolog.enuns;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_PSICOLOGO
+}
